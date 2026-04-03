aa4d5ece05f3201a649d94ab3083277f78931ceb
Liam Rice <liam.luc.rice@gmail.com>
2026-04-03 11:43:04 +0200

    Made some updates for a better simulation, maybe work on even more abstract model since the economy is not the main focus. Put more detail in the defense industrial complex?

--- src/objects_2.py
+++ src/objects_2.py
@@ -18,22 +18,23 @@ class EducationType(Enum):
 
 
 class IndustryType(Enum):
-    # Agriculture
+    # Raw resources
     SUBSISTENCE = 0
     AGRICULTURE = 1
+    MINING = 2
     # Civilian Manufacturing
-    CONSUMER_GOODS = 2
-    METALLURGY = 3
-    VEHICLES = 4
-    SHIPS = 5
-    AIRCRAFT = 6
-    SEMICONDUCTORS = 7
-    CONSTRUCTION = 8
+    CONSUMER_GOODS = 3
+    METALLURGY = 4
+    VEHICLES = 5
+    SHIPS = 6
+    AIRCRAFT = 7
+    SEMICONDUCTORS = 8
+    CONSTRUCTION = 9
     # Military Manufacturing
-    MILITARY_GOODS = 9
-    AMMUNITION = 10
+    MILITARY_GOODS = 10
+    AMMUNITION = 11
     # Services
-    SERVICES = 11
+    SERVICES = 12
 
 
 class Good:
@@ -83,25 +84,6 @@ class Population:
                 next_level.number += to_promote
 
 
-class Market:
-
-    def __init__(self, goods:list[Good]):
-        self.market = goods
-    
-    def update_demand(self, population:list[Population], industries:list[Industry]):
-        for good in self.market:
-            good.demand = 0
-            for pop in population:
-                for required_good, quantity in pop.required_goods:
-                    if required_good == good.name:
-                        good.demand += pop.number * quantity
-                for optional_good, quantity in pop.optional_goods:
-                    if optional_good == good.name:
-                        good.demand += pop.number * quantity
-    
-
-
-
 class Industry:
     
     def __init__(self, name:str, industry_type:IndustryType, base_productivity:float, technology_level:float, avg_salary:float, num_jobs:int, required_education: EducationLevel, average_production_value:float, input_goods:list[dict]=[]):
@@ -121,6 +103,9 @@ class Industry:
         self.productivity = self.calculate_productivity()
         self.input_goods = input_goods
         self.capital = 0
+        self.stock = 0
+        self.demand = 0
+        self.demand_growth = 0
     
     def update_avg_pop_education(self):
         total_employed = 0
@@ -144,7 +129,7 @@ class Industry:
         education_modifier = self.avg_pop_education / self.required_education.value
         if education_modifier > 1.0: education_modifier = math.cbrt(education_modifier)
         technology_modifier = 1.0 + (self.technology_level * 0.2)
-        productivity = self.base_productivity * education_modifier * technology_modifier * self.employment_rate
+        productivity = self.base_productivity * education_modifier * technology_modifier
         return productivity + productivity * sum(self.productivity_bonuses)
 
     def update_hiring(self, population:list[Population]):
@@ -192,62 +177,127 @@ class Industry:
         return amount
     
     def production(self) -> float:
-        return self.calculate_productivity() * self.num_jobs
+        return self.calculate_productivity() * self.total_hired
     
-    def _input_costs(self) -> float:
+    def _input_costs(self, industries:list) -> float:
         costs = 0.0
         for input_good in self.input_goods:
             for good_name, details in input_good.items():
-                costs += details["quantity"] * details["cost"]
+                for industry in industries:
+                    if industry.industry_type == good_name:
+                        costs += details["quantity"] * industry.avg_production_value
         return costs
     
     def income(self) -> float:
         return self.production() * self.avg_production_value
     
-    def expenses(self) -> tuple[float, float]:
+    def expenses(self, industries:list) -> tuple[float, float]:
         salary = self.avg_salary * self.total_hired
-        materials = self.production() * self._input_costs()
+        materials = self.production() * self._input_costs(industries)
         return salary, materials
     
-    def gdp_contribution(self) -> float:
-        return self.income() - self.expenses()[1]
+    def gdp_contribution(self, industries:list) -> float:
+        return self.income() - self.expenses(industries)[1]
 
-    def profitability(self) -> float:
-        return self.income() - sum(self.expenses())
-
-    def optimise_production(self, market):
-        pass
+    def profitability(self, industries:list) -> float:
+        return self.income() - sum(self.expenses(industries))
     
-    def calculate_optimal_salary(self):
-        budget = 0.8 * (self.income() - self._input_costs() * self.production())
+    def optimise_production(self):
+        productivity = self.calculate_productivity()
+        change = 1.1 if self.demand > self.stock else 0.9
+        required_jobs = (self.demand_growth * change) // productivity if productivity > 0 else self.num_jobs
+        self.num_jobs = max(1, int(required_jobs))
+    
+    def calculate_optimal_salary(self, industries:list):
+        budget = 0.8 * (self.income() - self._input_costs(industries) * self.production())
         optimal_salary = budget / self.num_jobs
         self.avg_salary = optimal_salary
     
-    def produce(self, market):
+    def buy_inputs(self, industries:list) -> float:
+        production_multiplier = 1.0
+        total_cost = 0.0
+        purchase_percentages = []
+        for input_good in self.input_goods:
+            for good_name, details in input_good.items():
+                for industry in industries:
+                    if industry.industry_type == good_name:
+                        percentage_cost = details["quantity"] * industry.avg_production_value
+                        purchase_percentages.append(percentage_cost)
+        for input_good in self.input_goods:
+            for good, percentage in zip(input_good.items(), purchase_percentages):
+                good_name, details = good
+                required_quantity = details["quantity"] * self.production()
+                for industry in industries:
+                    if industry.industry_type == good_name:
+                        resources_available =  min(required_quantity, industry.stock)
+                        resource_budget = (percentage / sum(purchase_percentages)) if sum(purchase_percentages) > 0 else 1
+                        max_purchasable = (self.capital * resource_budget) / industry.avg_production_value
+                        resources_available = min(resources_available, max_purchasable)
+                        if resources_available < required_quantity:
+                            production_multiplier = min(production_multiplier, resources_available / required_quantity)
+                        cost = resources_available * industry.avg_production_value
+                        total_cost += cost
+                        industry.stock -= min(required_quantity, industry.stock)
+                        self.capital -= cost
+        return production_multiplier
+    
+    def produce(self, industries:list):
         production = self.production()
-        for good in market.market:
-            if good.name == self.industry_type:
-                good.quantity += production
-                good.production = production
-                self.capital += self.income() - sum(self.expenses())
+        production_modifier = self.buy_inputs(industries)
+        self.stock += production * production_modifier
 
-    def update(self, population:list[Population], market:Market):
-        self.produce(market)
-        self.optimise_production(market)
-        self.calculate_optimal_salary()
+    def sell(self):
+        price_change = 0.0
+        if self.demand != self.stock:
+            price_change = 0.1 * (self.demand - self.stock) / self.demand if self.demand > 0 else 0
+        to_sell = min(self.stock, self.demand)
+        self.stock -= to_sell
+        self.demand -= to_sell
+        self.capital += to_sell * self.avg_production_value
+        if price_change != 0.0:
+            self.avg_production_value *= (1 + price_change)
+    
+    def update_demand(self, population:list[Population], industries:list) -> int:
+        demand_growth = 0
+        for pop in population:
+            for good, quantity in pop.required_goods:
+                if good == self.industry_type:
+                    self.demand += pop.number * quantity
+                    demand_growth += pop.number * quantity
+            for good, quantity in pop.optional_goods:
+                if good == self.industry_type:
+                    self.demand += pop.number * quantity
+                    demand_growth += pop.number * quantity
+        for industry in industries:
+            for input_good in industry.input_goods:
+                for good_name, details in input_good.items():
+                    if good_name == self.industry_type:
+                        self.demand += industry.production() * details["quantity"]
+                        demand_growth += industry.production() * details["quantity"]
+        return demand_growth
+
+    def update(self, population:list[Population], industries:list):
+        self.demand_growth = self.update_demand(population, industries)
+        self.produce(industries)
+        self.sell()
+        self.optimise_production()
+        self.calculate_optimal_salary(industries)
         self.update_hiring(population)
     
     def __str__(self):
-        return f"Industry: {self.name}\nType: {self.industry_type.name}\nTech Level: {self.technology_level}\nAvg Salary: {self.avg_salary}\nJobs: {self.num_jobs}\nEmployed: {self.total_hired}\nProductivity: {self.calculate_productivity():.2f}\nIncome: {self.income():.2f}\nExpenses: {self.expenses()[0]:.2f} (Salary), {self.expenses()[1]:.2f} (Materials)\nProfitability: {self.profitability():.2f}"
+        return f"Industry: {self.name}\nType: {self.industry_type.name}\nTech Level: {self.technology_level}\nAvg Salary: {self.avg_salary}\nJobs: {self.num_jobs}\nEmployed: {self.total_hired}\nProductivity: {self.productivity:.2f}\nIncome: {self.income():.2f}"
+    
+    def print_self(self, industries:list):
+        salary, material_costs = self.expenses(industries)
+        print(f"Industry: {self.name}\nType: {self.industry_type.name}\nTech Level: {self.technology_level}\nAvg Salary: {self.avg_salary}\nJobs: {self.num_jobs}\nEmployed: {self.total_hired}\nProductivity: {self.productivity:.2f}\nIncome: {self.income():.2f}\nSalary Costs: {salary:.2f}\nMaterial Costs: {material_costs:.2f}\nProduction: {self.production():.2f}\nStock: {self.stock:.2f}\nDemand: {self.demand:.2f}\nCapital: {self.capital:.2f}")
 
 
 class Nation:
     
-    def __init__(self, name:str, population:list[Population], industries:list[Industry], market:Market, growth_rate:float=3, mortality_rate:float=1, life_expectancy:float=50.0, age_of_majority:int=18):
+    def __init__(self, name:str, population:list[Population], industries:list[Industry], growth_rate:float=3, mortality_rate:float=1, life_expectancy:float=50.0, age_of_majority:int=18):
         self.name = name
         self.population = population
         self.industries = industries
-        self.market = market
         self.growth_rate = growth_rate
         self.mortality_rate = mortality_rate
         self.life_expectancy = life_expectancy
@@ -268,14 +318,13 @@ class Nation:
             return 0.0
         return (total_population - self.get_total_employed()) / total_population
     
-    def update_employement(self):
-        self.industries.sort(key=lambda ind: ind.avg_salary, reverse=True)
+    def update_employment(self):
         for industry in self.industries:
             industry.update_hiring(self.population)
-    
+
     def update_population(self):
         total_population = self.get_total_population()
-        births = total_population * (self.growth_rate / (2 * self.life_expectancy))
+        births = math.floor(total_population * (self.growth_rate / (2 * self.life_expectancy)))
         death_rate = self.mortality_rate / self.life_expectancy
         total_deaths = 0
         for pop in self.population:
@@ -283,9 +332,14 @@ class Nation:
                 pop.number += births
                 pop.promote_pops(self.age_of_majority, self.population)
             else:
-                num_deaths = pop.number * death_rate
+                num_deaths = math.floor(pop.number * death_rate)
                 total_deaths += num_deaths
                 pop.kill(num_deaths, self.industries)
+    
+    def update_industries(self):
+        self.industries.sort(key=lambda ind: ind.avg_salary, reverse=True)
+        for industry in self.industries:
+            industry.update(self.population, self.industries)
     
     def __str__(self):
         return f"Nation: {self.name}\nPopulation: {self.get_total_population()}\nUnemployment Rate: {self.get_unemployment_rate()*100:.2f}%\nIndustries:\n" + "\n".join([f"  {ind.name} (Jobs: {ind.num_jobs}, Employed: {ind.total_hired}, Productivity: {ind.calculate_productivity():.2f})" for ind in self.industries])
--- test.py
+++ test.py
@@ -5,16 +5,16 @@ from src.objects_2 import *
 if __name__ == "__main__":
 
     # Populations
-    pop0 = Population(200, EducationLevel.CHILD)
-    pop1 = Population(800, EducationLevel.UNEDUCATED)
-    pop2 = Population(500, EducationLevel.EDUCATED)
-    pop3 = Population(200, EducationLevel.HIGHLY_EDUCATED)
+    pop0 = Population(300, EducationLevel.CHILD)
+    pop1 = Population(1800, EducationLevel.UNEDUCATED)
+    pop2 = Population(1000, EducationLevel.EDUCATED)
+    pop3 = Population(500, EducationLevel.HIGHLY_EDUCATED)
 
     # Industries
     industry1 = Industry(
         "Agriculture",
         IndustryType.AGRICULTURE,
-        base_productivity=25,
+        base_productivity=50,
         technology_level=1,
         avg_salary=150,
         num_jobs=1000,
@@ -25,62 +25,80 @@ if __name__ == "__main__":
     industry2 = Industry(
         "Manufacturing",
         IndustryType.CONSUMER_GOODS,
-        base_productivity=10,
+        base_productivity=50,
         technology_level=1,
         avg_salary=200,
         num_jobs=500,
         required_education=EducationLevel.UNEDUCATED,
         average_production_value=30.0,
         input_goods=[
-            {"One": {
-                "quantity": 1,
-                "cost": 12.0
+            {IndustryType.AGRICULTURE: {
+                "quantity": 2,
             }}
         ]
+    )
+    industryX = Industry(
+        "Metallurgy",
+        IndustryType.METALLURGY,
+        base_productivity=20,
+        technology_level=1,
+        avg_salary=200,
+        num_jobs=300,
+        required_education=EducationLevel.UNEDUCATED,
+        average_production_value=3.0,
+        input_goods=[]
+    )
+    industryY = Industry(
+        "Services",
+        IndustryType.SERVICES,
+        base_productivity=10,
+        technology_level=1,
+        avg_salary=500,
+        num_jobs=400,
+        required_education=EducationLevel.EDUCATED,
+        average_production_value=200.0,
+        input_goods=[]
     )
     industry3 = Industry(
         "Vehicles",
         IndustryType.VEHICLES,
-        base_productivity=0.2,
+        base_productivity=1,
         technology_level=1,
         avg_salary=300,
         num_jobs=200,
         required_education=EducationLevel.EDUCATED,
         average_production_value=2000.0,
         input_goods=[
-            {"One": {
-                "quantity": 1,
-                "cost": 250.0
+            {IndustryType.METALLURGY: {
+                "quantity": 25,
             }},
-            {"Two": {
-                "quantity": 0.2,
-                "cost": 800.0
+            {IndustryType.SERVICES: {
+                "quantity": 3,
             }}
         ]
     )
 
-    # Market
-    good1 = Good(IndustryType.AGRICULTURE, base_price=6.0)
-    good2 = Good(IndustryType.CONSUMER_GOODS, base_price=30.0)
-    good3 = Good(IndustryType.VEHICLES, base_price=2000.0)
-    market = Market([good1, good2, good3])
-
     # Nation
-    nation = Nation("Testland", [pop0, pop1, pop2, pop3], [industry1, industry2, industry3], market)
+    nation = Nation("Testland", [pop0, pop1, pop2, pop3], [industry1, industry2, industry3, industryY, industryX])
 
 
     #=== TEST ===#
     print("===== INITIAL STATE =====")
     print(nation)
+    nation.update_employment() # initial employment state
 
-    print("\n===== AFTER UPDATING EMPLOYMENT =====")
-    nation.update_employement()
-    print(nation)
+    for i in range(100):
+        print(f"\n===== UPDATE CYCLE {i+1} =====")
 
-    print("\n===== AFTER UPDATING INDUSTRIES =====")
-    for ind in nation.industries:
-        print("\n"+str(ind))
+        print("\n===== AFTER UPDATING EMPLOYMENT =====")
+        nation.update_industries()
+        print(nation)
 
-    print("\n===== AFTER UPDATING POPULATION =====")
-    nation.update_population()
-    print(nation)
+        print("\n===== AFTER UPDATING INDUSTRIES =====")
+        for ind in nation.industries:
+            print("")
+            ind.print_self(nation.industries)
+
+        print("\n===== AFTER UPDATING POPULATION =====")
+        nation.update_population()
+        print(nation)
